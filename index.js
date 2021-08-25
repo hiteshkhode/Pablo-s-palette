7996cb648eeed8629c4efc57b5dff786ec1ec150
hiteshkhode <khodehitesh@gmail.com>
2021-08-25 15:25:52 +0000

    refreshinguserpalettes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const crypto = require('crypto')
 require('dotenv').config()
 
+var email, password
 const app = express()
 app.use(express.static('public'));
 app.use(express.json());
@@ -12,20 +13,22 @@ var conn = mongoose.connection;
 conn.on('connected', function(){
     console.log('its connected')
 })
-const signuptableschema = new mongoose.Schema({
+const signuptableschemaref = new mongoose.Schema({
     email: String,
     password: String,
     userid: String,
-    palette: [{red : Number, green: Number, blue: Number}]
+    palettes: Array,
+    teststring: String
 })
 
-const signuptable = mongoose.model("signuptable", signuptableschema);
+const signuptable = mongoose.model("signuptable", signuptableschemaref);
 
 function personadder(req, res){
     console.log('in personadder', req.body.email)
     const signuprow = new signuptable({
         email: req.body.email,
-        password: crypto.createHash('md5').update(req.body.password).digest('hex')
+        password: crypto.createHash('md5').update(req.body.password).digest('hex'),
+        userid: req.body.email
     })
     signuptable.find({email: req.body.email}, (err, signuptableres) => {
         console.log(signuptableres)
@@ -38,6 +41,8 @@ function personadder(req, res){
 }
 function login(req, res){
     console.log('in login')
+    email = req.body.email
+    password = req.body.password
     signuptable.find({email: req.body.email}, (err, signuptableres) => {
         if(signuptableres[0] === undefined) {
             res.send({fallout: 'user doesnt exist on database'})
@@ -45,6 +50,28 @@ function login(req, res){
         }
         if(signuptableres[0].password === crypto.createHash('md5').update(req.body.password).digest('hex')) res.send({fallout: 'successul login'})
         else res.send({fallout: 'bad credentials'})
+    })
+}
+function savepalette(req, res){
+    console.log(req.body.email, req.body.palettestoupload)
+    signuptable.updateOne({email: req.body.email}, {palettes: req.body.palettestoupload}, (err) => {
+        if(err) console.log(err)
+        else console.log('success os updating')
+    })
+    signuptable.find({email: req.body.email}, (err, signuptableres) => {
+        if(err) console.log(err)
+        else{
+            console.log('inside else')
+            console.log(signuptableres)
+        }
+    })
+}
+function getuserpalettes(req, res){
+    signuptable.find({email: req.body.email}, (err, signuptableres) => {
+        if(err) console.log(err)
+        else{
+            res.send({fallout: signuptableres[0].palettes})
+        }
     })
 }
 
@@ -58,6 +85,8 @@ app.get('/', (req, res) => {
 })
 app.post('/personadder', personadder)
 app.post('/login', login)
+app.post('/savepalette', savepalette)
+app.post('/getuserpalettes', getuserpalettes)
 
 app.listen('3000', () => {
     console.log('communicating with 3000')
--- public/experimental.js
+++ public/experimental.js
@@ -1,62 +1,62 @@
-// var email, password
-// var inflagcheck = 'login'
+var email, password
+var inflagcheck = 'login'
 
-// function loginflagupdater(event){
-//     if(event.target.getAttribute('id') === 'loginbutton'){
-//         inflagcheck = 'login'
-//         document.getElementById('loginbutton').style.color = 'black'
-//         document.getElementById('signinbutton').style.color = 'gray'
-//     }
-//     else{
-//         inflagcheck = 'signup'
-//         document.getElementById('loginbutton').style.color = 'gray'
-//         document.getElementById('signinbutton').style.color = 'black'
-//     }
-//     console.log(inflagcheck)
-// }
+function loginflagupdater(event){
+    if(event.target.getAttribute('id') === 'loginbutton'){
+        inflagcheck = 'login'
+        document.getElementById('loginbutton').style.color = 'black'
+        document.getElementById('signinbutton').style.color = 'gray'
+    }
+    else{
+        inflagcheck = 'signup'
+        document.getElementById('loginbutton').style.color = 'gray'
+        document.getElementById('signinbutton').style.color = 'black'
+    }
+    console.log(inflagcheck)
+}
 
-// document.getElementById("signupsubmit").addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     console.log('im here', document.getElementById('emailinput').value)
-//     email = document.getElementById('emailinput').value;
-//     password = document.getElementById('passwordinput').value
-//     if(inflagcheck === 'signup'){
-//         fetch('/personadder',{
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({email, password}),
-//             }).then(data => data.json().then(msg => processalert(msg.fallout)))
-//     }
-//     else{
-//         fetch('/login',{
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({email, password}),
-//             }).then(data => data.json().then((msg) => {
-//                 processalert(msg.fallout);
-//                 if(msg.fallout === 'successul login') renderapp()
-//             }))
-//     }
-// })
+document.getElementById("signupsubmit").addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log('im here', document.getElementById('emailinput').value)
+    email = document.getElementById('emailinput').value;
+    password = document.getElementById('passwordinput').value
+    if(inflagcheck === 'signup'){
+        fetch('/personadder',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email, password}),
+            }).then(data => data.json().then(msg => processalert(msg.fallout)))
+    }
+    else{
+        fetch('/login',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email, password}),
+            }).then(data => data.json().then((msg) => {
+                processalert(msg.fallout);
+                if(msg.fallout === 'successul login') renderapp()
+            }))
+    }
+})
 
-// function processalert(msg){
-//     msgbox = document.getElementsByClassName('processalert')[0]
-//     msgbox.setAttribute('id', '')
-//     document.getElementById('processalerttext').innerText = msg
-// }
-// function closemsgbox(){
-//     document.getElementsByClassName('processalert')[0].setAttribute('id', 'none')
-// }
-
+function processalert(msg){
+    msgbox = document.getElementsByClassName('processalert')[0]
+    msgbox.setAttribute('id', '')
+    document.getElementById('processalerttext').innerText = msg
+}
+function closemsgbox(){
+    document.getElementsByClassName('processalert')[0].setAttribute('id', 'none')
+}
 
 ///////////////////////////////////////////////////////BELOW CODE IS FOR APP//////////////////////////////////////////////////////////
 
-renderapp()
 function renderapp(){
+    document.getElementsByClassName('signupbox')[0].setAttribute('id', 'none')
+    document.getElementsByClassName('maincontent')[0].setAttribute('id', 'maincontent')
     renderinitialcolors()
 }
 function renderinitialcolors(){
@@ -67,10 +67,71 @@ function renderinitialcolors(){
 }
 function coloreddivgenerator(){
     div = document.createElement('div')
+    div.innerHTML = '<button onclick="deleteperticularcolor(event)">remove</button>'
     div.setAttribute('class', 'randomcolor')
     div.style.background = 'rgb(' + Math.random()*256 + ', ' + Math.random()*256 + ', ' + Math.random()*256 + ')'
     document.getElementById('colorsofpalette').appendChild(div)
 }
-function addpalettetodb(){
-    fetch('/addpalettetodb')
+function updatelocalpalette(){
+    stagedpalette = document.getElementById('colorsofpalette')
+    cloneofstaged = stagedpalette.cloneNode(true)
+    cloneofstaged.setAttribute('class', 'perticularuserspalettes')
+    cloneofstaged.setAttribute('id', '')
+    document.getElementById('userspalettes').appendChild(cloneofstaged)
 }
+function deleteperticularcolor(event){
+    element = event.target.parentNode 
+    element.parentNode.removeChild(element)
+}
+function savepalette(){
+    palettestoupload = []
+    perticularpalettetoupload = []
+    for (let i = 0; i < document.getElementsByClassName('perticularuserspalettes').length; i++) {
+        outerpalette = document.getElementsByClassName('perticularuserspalettes')[i].childNodes
+        for (let j = 0; j < outerpalette.length; j++) {
+            perticularpalettetoupload.push(outerpalette[j].style.backgroundColor)
+        }
+        palettestoupload.push(perticularpalettetoupload)
+        perticularpalettetoupload = []
+    }
+    console.log(palettestoupload)
+    fetch('/savepalette', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email, palettestoupload}),
+        })
+}
+function refreshuserpalette(){
+    document.getElementById('userspalettes').innerHTML = '<button onclick="savepalette()">upload</button><button onclick="refreshuserpalette()">refresh</button>'
+    fetch('/getuserpalettes', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email}),
+        }).then(data => data.json().then((userpalettes) => {
+            console.log(userpalettes.fallout)
+            colorsforuserpalette = userpalettes.fallout
+            for (let i = 0; i < colorsforuserpalette.length; i++) {
+                let foroneuserpalette = document.createElement('div')
+                foroneuserpalette.setAttribute('class', 'perticularuserspalettes')
+                for (let j = 0; j < colorsforuserpalette[i].length; j++) {
+                    let colorinuserpalette = document.createElement('div')
+                    colorinuserpalette.setAttribute('class', 'randomcolor')
+                    colorinuserpalette.style.background = colorsforuserpalette[i][j]
+                    foroneuserpalette.appendChild(colorinuserpalette)
+                }
+                document.getElementById('userspalettes').appendChild(foroneuserpalette)
+            }
+        }))
+}
+function userpalettedivgeneration(color){
+    div = document.createElement('div')
+    div.innerHTML = '<button onclick="deleteperticularcolor(event)">remove</button>'
+    div.setAttribute('class', 'randomcolor')
+    div.style.background = color
+    document.getElementById('colorsofpalette').appendChild(div)
+}
+
